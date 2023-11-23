031eb5ba78d827ad4b32fc746504e3e04e9a434b
VictorDutraa <149853348+VictorDutraa@users.noreply.github.com>
2023-11-23 16:30:21 -0300

    primeiro código inútil
    criei um arquivo dentro de m0-teste, entretanto, esqueci de colocar o formato ".c" no final dele, então o compilador não o reconheceu. Então, alterei para m0.c.

---
+++ m0-teste/m0.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int n1, n2;
+    printf("Digite dois números quaisquer e veja uma mágica:\n");
+    scanf("%d %d", &n1, &n2);
+    printf("Aqui estão os números:\n\n\nSUMIRAM!\n");
+    return 0;
+}
