67a986763d21ac411464f87f7cfea94276bff38a
VictorDutraa <149853348+VictorDutraa@users.noreply.github.com>
2023-11-23 16:42:28 -0300

    comentário de teste
    adicionei comentário pra testar o atalho do teclado pra dar commit e push.

--- m0-teste/m0.c
+++ m0-teste/m0.c
@@ -5,5 +5,6 @@ int main(){
     printf("Digite dois números quaisquer e veja uma mágica:\n");
     scanf("%d %d", &n1, &n2);
     printf("Aqui estão os números:\n\n\nSUMIRAM!\n");
+    //comentário só pra testar o commit e push através do atalho do teclado.
     return 0;
 }
